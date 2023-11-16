839fef7ccb792169cc6839abc74d3e895e9b1bbb
lelynguyen <lelynguyen2907@gmail.com>
2023-11-16 17:52:22 +0700

    add TGioHang.cs, GioHang.cshtml, _LayoutContent.cshtml

---
+++ Models/TGioHang.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanVaLi.Models;
+
+public partial class TGioHang
+{
+    public int Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public string MaSp { get; set; } = null!;
+
+    public decimal DonGia { get; set; }
+
+    public int SoLuong { get; set; }
+
+    public virtual TDanhMucSp MaSpNavigation { get; set; } = null!;
+
+    public virtual TUser User { get; set; } = null!;
+}
